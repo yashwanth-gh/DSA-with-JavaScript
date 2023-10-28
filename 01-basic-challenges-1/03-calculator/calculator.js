42dd9fbffd1a7ca9cd8e3a74f84e35989490bc5f
Yashwanth B M <yashwanthbm36@gmail.com>
2023-10-28 17:17:09 +0530

    Day-1 | 3 Challanges

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
+function calculator(a,b,c) {
+    switch (c) {
+        case '+':
+            return a+b;
+            break;
+        case '-':
+            return a-b;
+            break;
+        case '*':
+            return a*b;
+            break;
+        case '/':
+            return a/b;
+            break;
+    
+        default:
+            console.log("invalid operator")
+            break;
+    }
+}
 
 module.exports = calculator;
