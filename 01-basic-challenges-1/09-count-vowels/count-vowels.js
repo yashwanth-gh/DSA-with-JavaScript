ab7aa984d26419f73a639ec0dd0a1fc5f3f12ba3
Yashwanth B M <yashwanthbm36@gmail.com>
2023-10-30 22:01:23 +0530

    Day-3 | Basic challenges-1 completed

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,19 @@
-function countVowels() {}
+function countVowels(str) {
+    const arr = str.toLowerCase().split('');
+    let count=0;
+    arr.forEach(e => {
+        if(isVowel(e))count++;
+    });
+    return count;
+}
+function isVowel(char){
+    const vowelCode = [97,101,105,111,117];
+    let code = char.charCodeAt(0);
+        if(code === 97 || code === 101 || code === 105 || code === 111 || code === 117){
+            return 1;
+        }
+    return 0;
+
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,9 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(!newArr.includes(arr[i]))newArr.push(arr[i]);
+    }
+    return newArr;
+}
 
 module.exports = removeDuplicates;
