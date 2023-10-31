592c72c331f2199bd2662fc75a3a3a37bd4c8476
Yashwanth B M <yashwanthbm36@gmail.com>
2023-10-31 15:52:30 +0530

    Day-3 | 4 Challenges

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,14 @@
-function findMissingNumber() {}
+function findMissingNumber(arr = undefined) {
+const len = arr.length;
+if(arr === undefined)return undefined;
+if(len === 0)return 1;
+const totalSum = ((len+1)*(len+2))/2;
+// let runningSum=0;
+// arr.forEach(e => {
+//     runningSum+=e;
+// });
+const runningSum = arr.reduce((sum,num)=>sum+num,0)
+return (totalSum-runningSum);
+}
 
 module.exports = findMissingNumber;
--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,7 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+    const set = new Set(str.split(''));
+    if(set.size !== str.length)return false;
+    return true;
+}
 
 module.exports = areAllCharactersUnique;
