cd86cc178f4c8812ef055bcbe0d31c16bc5e846d
Yashwanth B M <yashwanthbm36@gmail.com>
2023-10-31 15:52:30 +0530

    Day-4 | 4 Challenges

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,10 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+   const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   const start = alphabet.indexOf(arr[0]);
+   for(let i=0;i<arr.length;i++){
+    if(arr[i] !== alphabet[start+i])return alphabet[start+i];
+   }
+   return '';
+}
 
 module.exports = findMissingLetter;
--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,22 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    let charCount = new Map;
+    for(const char of str){
+        charCount.set(char,(charCount.get(char) || 0)+1);
+    }
+for(const char of str){
+    if(charCount.get(char)===1)return char;
+}
+return null;
+}
+// function findFirstNonRepeatingCharacter(str) {
+//     let charCount = {};
+//     for(const char of str){
+//         charCount[char]= (charCount[char] || 0)+1;
+//     }
+// for(const char of str){
+//     if(charCount[char]===1)return char;
+// }
+// return null;
+// }
 
 module.exports = findFirstNonRepeatingCharacter;
