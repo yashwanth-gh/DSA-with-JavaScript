4fe595a38060a950764acf38549ef460d0540ce2
Yashwanth B M <yashwanthbm36@gmail.com>
2023-11-04 18:16:32 +0530

    Day-8 | 3 Challenges

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,11 @@
-function validatePassword() {}
+function validatePassword(str) {
+    const arr = str.split('');
+    const uppercaseCheck = arr.some(char=>/[A-Z]/.test(char));
+    const lowercaseCheck = arr.some(char=>/[a-z]/.test(char));
+    const digitCheck = arr.some(char=>/\d/.test(char));
+    // console.log(uppercaseCheck,lowercaseCheck,digitCheck,str.length)
+    if(uppercaseCheck && lowercaseCheck && digitCheck && str.length >=8)return true;
+    return false;
+}
 
 module.exports = validatePassword;
